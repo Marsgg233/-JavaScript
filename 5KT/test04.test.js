6f7a9d33faceb77270168061f8641776737aad9f
Marsel Gabbasov <marselgabb@gmail.com>
2025-05-26 04:06:21 +0300

    Update test04.test.js

--- 5KT/test04.test.js
+++ 5KT/test04.test.js
@@ -13,7 +13,7 @@ describe('Тестирование функции isFridayToday', () => {
         expect(isFridayToday()).toBe('Завтра пятница!');
     });
 
-    test('3', () => { //должен выводить "Завтра пятница!" при субботу
+    test('3', () => { //должен выводить "Завтра пятница!" при субботе
         const saturdayDate = new Date('2023-10-07T00:00:00Z');
         global.Date.now = jest.fn(() => saturdayDate.valueOf());
         expect(isFridayToday()).toBe('Пятница была вчера');
